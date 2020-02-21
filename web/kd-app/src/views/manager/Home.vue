4db13abea2251521c8bd639dc5fe16db887ea972
otatoz <279923215@qq.com>
2020-02-21 14:49:40 +0800

    看点资讯手机端框架

---
+++ web/kd-app/src/views/manager/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <!-- 头部 -->
+    <div class="header">
+      看点资讯精选
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data(){
+    return {
+      
+    }
+  },
+  created(){
+    
+  },
+  methods:{
+    
+  }
+}
+</script>
+<style scoped>
+.header {
+  height: 30px;
+  line-height: 30px;
+  background-color: #2E8FF4;
+  font-size: 16px;
+  color: white;
+  font-weight: bold;
+  text-align: center;
+}
+</style>
---
+++ web/kd-app/src/views/manager/User.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="user">
+    <div class="header">
+      
+    </div>
+    <van-cell title="个人信息" is-link to="/manager/address" />
+    <van-cell title="设置密码" is-link to="/manager/order" />
+  </div>
+</template>
+<script>
+export default {
+  methods:{
+    
+  }
+}
+</script>
+
+<style scoped>
+.header {
+  height: 200px;
+}
+
+
+</style>
