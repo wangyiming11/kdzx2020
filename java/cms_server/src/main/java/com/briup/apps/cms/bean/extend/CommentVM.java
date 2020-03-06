c3abe6662a8edc685db88a91d08f557f2044fe9a
AdminRDC <chen_2019@qq.com>
2020-03-06 14:29:22 +0800

    [任殿臣]:新增了部分接口

--- java/cms_server/src/main/java/com/briup/apps/cms/bean/extend/CommentVM.java
+++ java/cms_server/src/main/java/com/briup/apps/cms/bean/extend/CommentVM.java
@@ -14,12 +14,14 @@ package com.briup.apps.cms.bean.extend;
  * Reason:	 TODO ADD REASON. <br/>
  * Date:     2018年9月19日 上午10:33:04 <br/>
  * @author   lichunyu
- * @version  
+ * @version
  * @since    JDK 1.6
- * @see 	 
+ * @see
  */
 
 import com.briup.apps.cms.bean.Article;
+import com.briup.apps.cms.bean.Comment;
+import com.briup.apps.cms.bean.User;
 
 public class CommentVM {
 	private Long id;
@@ -27,8 +29,9 @@ public class CommentVM {
 	private String status;
 	private String content;
 	private String customerId;
-	
+
 	private Article article;
+	private User customer;
 
 	public Long getId() {
 		return id;
@@ -37,6 +40,8 @@ public class CommentVM {
 	public void setId(Long id) {
 		this.id = id;
 	}
+
+
 
 	public String getCommenttime() {
 		return commenttime;
@@ -78,4 +83,11 @@ public class CommentVM {
 		this.article = article;
 	}
 
+	public User getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(User customer) {
+		this.customer = customer;
+	}
 }
--- java/cms_server/src/main/java/com/briup/apps/cms/web/controller/manager/UserController.java
+++ java/cms_server/src/main/java/com/briup/apps/cms/web/controller/manager/UserController.java
@@ -8,36 +8,35 @@
 
 package com.briup.apps.cms.web.controller.manager;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 import com.baidu.ai.aip.utils.IdentificationUtil;
-import com.briup.apps.cms.bean.CMSFile;
-import com.briup.apps.cms.util.FastDFS;
+import com.briup.apps.cms.bean.User;
+import com.briup.apps.cms.bean.extend.UserExtend;
+import com.briup.apps.cms.service.impl.UserService;
+import com.briup.apps.cms.vm.UserVM;
+import com.zhenzi.sms.ZhenziSmsClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.*;
 
-import com.briup.apps.cms.bean.User;
 import com.briup.apps.cms.service.ICMSUserService;
 import com.briup.apps.cms.util.MsgResponse;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.ServletContext;
 
 /**
  * ClassName:UserController <br/>
  * Function: TODO ADD FUNCTION. <br/>
  * Reason:	 TODO ADD REASON. <br/>
  * Date:     2018年9月18日 下午4:04:16 <br/>
+ *
  * @author lichunyu
- * @version
- * @since JDK 1.6
  * @see
+ * @since JDK 1.6
  */
 @Api(description = "用户管理")
 @RestController
@@ -45,6 +44,10 @@ import javax.servlet.http.HttpServletRequest;
 public class UserController {
     @Autowired
     private ICMSUserService cmsUserService;
+    @Autowired
+    private ServletContext context;
+    @Autowired
+    UserService userService;
 
     @ApiOperation(value = "查询所有用户信息")
     @GetMapping(value = "findAllUser")
@@ -68,6 +71,18 @@ public class UserController {
         }
     }
 
+    @ApiOperation(value = "通过用户id查询用户信息")
+    @GetMapping(value = "findUserById")
+    public MsgResponse findUserById(int id) {
+        try {
+            User user = cmsUserService.findUserById(id);
+            return MsgResponse.success("success", user);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return MsgResponse.error(e.getMessage());
+        }
+    }
+
     @ApiOperation(value = "通过用户名查询用户信息")
     @GetMapping(value = "findUserByUsername")
     public MsgResponse findUserByUsername(String username) {
@@ -80,17 +95,17 @@ public class UserController {
         }
     }
 
-    @ApiOperation(value = "保存或更新用户信息", notes = "如果参数中id不为空表示保存，否则表示更新")
-    @PostMapping(value = "saveOrUpdateUser")
-    public MsgResponse saveOrUpdateUser(User user) {
-        try {
-            cmsUserService.saveOrUpdate(user);
-            return MsgResponse.success("success", user);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return MsgResponse.error(e.getMessage());
-        }
-    }
+//    @ApiOperation(value = "保存或更新用户信息", notes = "如果参数中id不为空表示保存，否则表示更新")
+//    @PostMapping(value = "saveOrUpdateUser")该接口已停用
+//    public MsgResponse saveOrUpdateUser(User user) {
+//        try {
+//            cmsUserService.saveOrUpdate(user);
+//            return MsgResponse.success("success", user);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            return MsgResponse.error(e.getMessage());
+//        }
+//    }
 
     @ApiOperation(value = "修改用户状态", notes = "true表示开启，false表示关闭")
     @PostMapping(value = "changeStatus")
@@ -104,8 +119,87 @@ public class UserController {
         }
     }
 
+    @ApiOperation(value = "发送短信验证码", notes = "0.06元/次，请谨慎测试 text字段填写要发送的内容")
+    @PostMapping(value = "getSmsCode")
+    public MsgResponse getSmsCode(String phoneNum, String text) {
+        try {
+            ZhenziSmsClient client = new ZhenziSmsClient("https://sms_developer.zhenzikj.com", "104584", "aca1366a-d551-4e8a-80fa-1292cf4fd6b6");
+            Map<String, String> params = new HashMap<String, String>();
+            params.put("message", text);
+            params.put("number", phoneNum);
+            String result = client.send(params);
+            String balance = client.balance();
+//            HashMap map = new HashMap<String, String>();
+//            map.putAll(IdentificationUtil.getSmsByJson(result));
+//            map.putAll(IdentificationUtil.getSmsNumByJson(balance));
+            return MsgResponse.success("success", result + balance);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return MsgResponse.error(e.getMessage());
+        }
+    }
 
+    @ApiOperation(value = "用户身份证识别", notes = "需传身份证照片url eg  " +
+            "idcard_front: https://ai.bdstatic.com/file/D348C98B677149C8B95909CFA204BA1C   " +
+            "idcard_back: https://ai.bdstatic.com/file/75537E3E7EE8446EBDCB35B5CEDEB9B6")
+    @PostMapping(value = "identificationUser")
+    public MsgResponse identificationUser(String idcard_front, String idcard_back) {
+        try {
+            byte[] front_data = IdentificationUtil.getImageFromURL(idcard_front);
+            byte[] back_data = IdentificationUtil.getImageFromURL(idcard_back);
 
+            String front = IdentificationUtil.idcard("front", front_data);
+            String back = IdentificationUtil.idcard("back", back_data);
+            HashMap map = new HashMap<String, String>();
+            map.putAll(IdentificationUtil.getFrontByJson(front));
+            map.putAll(IdentificationUtil.getBackByJson(back));
 
+            return MsgResponse.success("success", map);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return MsgResponse.error(e.getMessage());
+        }
+    }
+
+    @PostMapping(path = "login")
+    @ApiOperation("登录")
+    public MsgResponse login(@RequestBody UserVM userVM) throws Exception {
+        User user = cmsUserService.findUser(userVM.getUsername(), DigestUtils.md5DigestAsHex(userVM.getPassword().getBytes()), userVM.getRole());
+        if (user != null) {
+            String uuid = UUID.randomUUID().toString().replaceAll("-", "");
+            context.setAttribute(uuid, user);
+            Map<String, String> map = new HashMap<>();
+            map.put("token", uuid);
+            return MsgResponse.success("success", map);
+        } else {
+            return MsgResponse.error("用户信息不符");
+        }
+        // 消息验证
+    }
+
+    @GetMapping("info")
+    @ApiOperation("根据token获取用户信息")
+    public MsgResponse info(String token) throws Exception {
+        Object obj = context.getAttribute(token);
+        if (obj != null && obj instanceof User) {
+            User user = (User) obj;
+            return MsgResponse.success("success", user);
+        }
+        return MsgResponse.error("token失效，请重新登录");
+
+    }
+
+    @PostMapping("reg")
+    @ApiOperation(value="用户注册、更新接口",  notes = "如果参数中id不为空表示保存，否则表示更新")
+    public MsgResponse reg(User user) {
+        String result = userService.reg(user);
+        return MsgResponse.success(result, user);
+    }
+
+    @PostMapping("logout")
+    @ApiOperation("退出")
+    public MsgResponse logout() throws Exception {
+        return MsgResponse.success("success", "");
+    }
 }
 
